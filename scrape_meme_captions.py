036ca3860cab6179671e7ea8b922a31f65338758
George Botev <gbotev@gbotev.home>
2020-11-19 11:51:31 -0500

    Added meme template and caption scraper

---
+++ scrape_meme_captions.py
@@ -0,0 +1,56 @@
+# Adapted from: https://github.com/ruhomor/Meme-Generator/blob/master/data_scraper.py
+
+from bs4 import BeautifulSoup as bs
+from bs4 import SoupStrainer as ss
+from concurrent.futures import ThreadPoolExecutor
+import re
+import requests
+import itertools
+import argparse
+import pandas as pd
+
+base_url = 'https://memegenerator.net'
+meme_template_url = f'{base_url}/memes/popular/alltime/'
+
+def get_bs(url, parse_only=None):
+  r = requests.get(url)
+  return bs(r.text, 'html.parser', parse_only=parse_only)
+
+def save_meme_template(save_dir, url):
+  r = requests.get(url, stream=True)
+  name = url.split('/')[-1]
+  with open(f'{save_dir}/{name}', 'wb') as outfile:
+    outfile.write(r.content)
+
+def process_meme_template_page(meme_template, j):
+  dfs = []
+  meme_href = meme_template.find('a')['href']
+  page_url = f"{base_url}/{meme_href}" if j == 0 else f"{base_url}/{meme_href}/images/popular/alltime/page/{str(j + 1)}"
+  memes = get_bs(page_url, parse_only=ss('a', href=re.compile('instance')))
+  for meme in memes:
+    s = get_bs(f"{base_url}/{meme.get('href')}", parse_only=ss(class_='w100p'))
+    caption = s.find('img')['alt']
+    dfs.append(pd.DataFrame([[meme_href[1:], caption.split('-')[1].lower()]], columns=['type', 'caption']))
+  return pd.concat(dfs, ignore_index=True)
+
+def main(n_pages_meme_types, n_pages_meme_egs, save_dir, outfile_name):
+  dfs = []
+  for i in range(n_pages_meme_types):
+    url = meme_template_url if i == 0 else f'{meme_template_url}/page/{str(i + 1)}'
+    meme_templates = get_bs(url, parse_only=ss(class_='char-img'))
+    for meme_template in meme_templates:
+      save_meme_template(save_dir, meme_template.find('img')['src'])
+    with ThreadPoolExecutor() as executor:
+      for df in executor.map(lambda x: process_meme_template_page(x[0], x[1]), itertools.product(meme_templates, range(n_pages_meme_egs))):
+        dfs.append(df)
+  df = pd.concat(dfs, ignore_index=True)
+  df.to_csv(f'{outfile_name}.csv')
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Helper script to download meme templates and captions from memegenerator.net')
+  parser.add_argument('n_pages_meme_types', type=int, help='number of meme template pages to scrape')
+  parser.add_argument('n_pages_meme_egs', type=int, help='number of memes per template to scrape')
+  parser.add_argument('--save_dir', type=str, default='meme_templates', help='local directory to save scraped meme templates')
+  parser.add_argument('--outfile_name', type=str, default='captions', help='CSV filename for scraped captions')
+  args = parser.parse_args()
+  main(args.n_pages_meme_types, args.n_pages_meme_egs, args.save_dir, args.outfile_name)
