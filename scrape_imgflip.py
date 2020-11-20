e3b458e6b9e2c7908738eaa1462f6672374625dc
George Botev <gbotev@gbotev.home>
2020-11-19 23:32:40 -0500

    Unfinished TODO

--- scrape_imgflip.py
+++ scrape_imgflip.py
@@ -8,11 +8,10 @@ import requests
 import argparse
 import pandas as pd
 
-base_url = 'https://memegenerator.net'
-meme_template_url = f'{base_url}/memes/popular/alltime/'
+base_url = 'https://imgflip.com/'
 
-def get_bs(url, parse_only=None):
-  r = requests.get(url)
+def get_bs(payload, parse_only=None):
+  r = requests.get(base_url, params=payload)
   return bs(r.text, 'html.parser', parse_only=parse_only)
 
 def save_meme_template(save_dir, url):
@@ -21,7 +20,7 @@ def save_meme_template(save_dir, url):
   with open(f'{save_dir}/{name}', 'wb') as outfile:
     outfile.write(r.content)
 
-def process_meme_template_page(meme_template, j):
+def process_month_page(month, j):
   try:
     dfs = []
     meme_href = meme_template.find('a')['href']
@@ -35,28 +34,21 @@ def process_meme_template_page(meme_template, j):
   except:
     return None
 
-def main(n_pages_meme_types, n_pages_meme_egs, save_dir, outfile_name):
+def main(n_pages_meme_types, n_pages, save_dir, outfile_name):
   dfs = []
-  for i in range(n_pages_meme_types):
-    url = meme_template_url if i == 0 else f'{meme_template_url}/page/{str(i + 1)}'
-    meme_templates = get_bs(url, parse_only=ss(class_='char-img'))
-    for j, meme_template in enumerate(meme_templates):
-      print(f'{i}-{j}')
-      save_meme_template(save_dir, meme_template.find('img')['src'])
-      with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(lambda i: process_meme_template_page(meme_template, i), i) for i in range(n_pages_meme_egs)]
-        for future in as_completed(futures):
-          future_result = future.result()
-          if future_result:
-            dfs.append(future_result)
+  for j in range(n_pages):
+    s = get_bs({'sort': 'top-2020-10', 'page': str(j + 1)}, parse_only=ss(class_='base-unit clearfix'))
+    for img in s.find_all('img', class_='base-img'):
+      raw_caption = img['alt']
+
   df = pd.concat(dfs, ignore_index=True)
   df.to_csv(f'{outfile_name}.csv')
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Helper script to download meme templates and captions from memegenerator.net')
   parser.add_argument('n_pages_meme_types', type=int, help='number of meme template pages to scrape')
-  parser.add_argument('n_pages_meme_egs', type=int, help='number of memes per template to scrape')
+  parser.add_argument('n_pages', type=int, help='number of memes per template to scrape')
   parser.add_argument('--save_dir', type=str, default='meme_templates', help='local directory to save scraped meme templates')
   parser.add_argument('--outfile_name', type=str, default='captions', help='CSV filename for scraped captions')
   args = parser.parse_args()
-  main(args.n_pages_meme_types, args.n_pages_meme_egs, args.save_dir, args.outfile_name)
+  main(args.n_pages_meme_types, args.n_pages, args.save_dir, args.outfile_name)
