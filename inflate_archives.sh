8113d2b172fd57b1af30c682d64ee8b86ad246e2
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-06 00:05:51 -0500

    Updated to use .tar extension to work on Linux

--- inflate_archives.sh
+++ inflate_archives.sh
@@ -1,2 +1,2 @@
-tar -xjvf data/11-25-20_21-1500.tsv.bz2 -C data/
-tar -xjvf data/meme_templates.bz2 -C data/
+tar -xjvf data/11-25-20_21-1500.tsv.tar.bz2 -C data/
+tar -xjvf data/meme_templates.tar.bz2 -C data/
