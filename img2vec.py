6e601ec5714761880f2838d3a95fbbd365d2b07d
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-06 19:56:40 -0500

    Less memory but still not an RNN

--- img2vec.py
+++ img2vec.py
@@ -1,7 +1,8 @@
 # Keep imports lightweight
 from PIL import Image
 from torchvision.models import wide_resnet101_2
-from os import path
+from os import path, isfile
+from os import remove as os_remove
 from csv import reader as csv_reader
 from csv import writer as csv_writer
 from torch import device, cuda, Tensor
@@ -18,11 +19,11 @@ from typing import List, Optional, Tuple
 
 class Wide_ResNet_101_2:
 
-    def __init__(self, data_dir: str, tsvname: str, outfile: str, captions: str, timeout: float, log_every: int, batch_size: int) -> None:
+    def __init__(self, data_dir: str, tsvname: str, out_dir: str, captions: str, timeout: float, log_every: int, batch_size: int) -> None:
         # Save parameters
         self.data_dir = data_dir
         self.tsvname = tsvname
-        self.outfile = outfile
+        self.out_dir = out_dir
         self.captions = captions
         self.timeout = timeout
         self.log_every = log_every
@@ -40,8 +41,8 @@ class Wide_ResNet_101_2:
                                      T.Normalize(mean=[0.485, 0.456, 0.406],
                                                  std=[0.229, 0.224, 0.225])])  # Recommended normalization for torchvision ImageNet pretrained models
         self.embeddings = []
-        self.model.avgpool.register_forward_hook(lambda m, m_in, m_out: self.embeddings.append(
-            m_out.data.detach().cpu().squeeze().numpy()))
+        self.model.avgpool.register_forward_hook(lambda input, output: self.embeddings.append(
+            output.data.detach().cpu().squeeze().numpy()))
 
     def get_tensor(self, line: List[str]) -> Optional[Tuple[Tensor, str]]:
         try:
@@ -62,7 +63,12 @@ class Wide_ResNet_101_2:
             return None
 
     def run(self) -> None:
+        # If captions TSV file already exists, then delete it
+        if isfile(path.join(self.data_dir, self.captions)):
+            os_remove(path.join(self.data_dir, self.captions))
+        # Switch logic based on CPU/GPU availability
         iters = 0
+        batches = 0
         if self.has_cuda:
             tensors = []
             # Append captions on the fly
@@ -87,6 +93,12 @@ class Wide_ResNet_101_2:
                                 del batch  # Free up GPU memory
                                 # Reset batch when done
                                 tensors = []
+                                # Save batch
+                                save(path.join(self.data_dir, self.out_dir,
+                                               f'{batches}.npy'), vstack(self.embeddings))
+                                # Reset embeddings to ease memory pressure
+                                self.embeddings = []
+                                batches += 1
                             if iters % self.log_every == 0:
                                 print(iters)
             # Check if incomplete batch present
@@ -95,7 +107,10 @@ class Wide_ResNet_101_2:
                 # Must have GPU in this branch
                 self.model(batch.to(device('cuda')))
                 del batch, tensors  # Might as well
-            save(path.join(self.data_dir, self.outfile), vstack(self.embeddings))
+                # Save incomplete batch
+                save(path.join(self.data_dir, self.out_dir,
+                               f'{batches}.npy'), vstack(self.embeddings))
+                self.embeddings = []  # Might as well
         else:
             # Append captions on the fly
             with open(path.join(self.data_dir, self.captions), 'a', newline='') as outfile:
@@ -117,14 +132,14 @@ class Wide_ResNet_101_2:
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Generates 2048-dimensional embeddings for images from Google's Conceptual Captions dataset using a pretrained Wide ResNet-101-2 neural network on ImageNet. Automatically uses (a single) GPU if available. Note that this program will append to the specified '--captions' TSV file, so make sure it does not already exist!",
+    parser = ArgumentParser(description="Generates 2048-dimensional embeddings for images from Google's Conceptual Captions dataset using a pretrained Wide ResNet-101-2 neural network on ImageNet. Automatically uses (a single) GPU if available, which is highly recommended as the CPU version currently has an extremely high memory overhead. Note that this program will append to the specified '--captions' TSV file, so it will delete it if it already exists!",
                             formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d', '--data_dir', type=str,
                         default='data', help='local data directory')
     parser.add_argument('-t', '--tsvname', type=str, default='gcc_full.tsv',
                         help='filename in local data directory of combined, detokenized GCC dataset captions')
-    parser.add_argument('-o', '--outfile', type=str, default='embeddings.npy',
-                        help='output filename to save in local data directory of embeddings of GCC dataset images')
+    parser.add_argument('-o', '--out_dir', type=str, default='embeddings',
+                        help='output directory of partial batch results of embeddings of GCC dataset images in local data directory')
     parser.add_argument('-c', '--captions', type=str, default='gcc_captions.tsv',
                         help='output filename to save in local data directory of GCC dataset captions corresponding to images that were actually embedded')
     parser.add_argument('-w', '--timeout', type=float, default=1.0,
@@ -135,5 +150,5 @@ if __name__ == "__main__":
                         help='GPU batch size to use if CUDA/GPU is available')
     args = parser.parse_args()
     model = Wide_ResNet_101_2(
-        args.data_dir, args.tsvname, args.outfile, args.captions, args.timeout, args.log_every, args.batch_size)
+        args.data_dir, args.tsvname, args.out_dir, args.captions, args.timeout, args.log_every, args.batch_size)
     model.run()
