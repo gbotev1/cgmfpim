2782c62395e70c9c3509b22eb180cc7d7feda804
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-09 18:40:23 -0500

    Less is more

--- inflate_archives.sh
+++ inflate_archives.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 tar -xvf data/meme_templates.tar -C data/
-lrunzip -cdivv data/11-25-20_21-1500.tsv.lrz -o data/meme_data.tsv
-lrunzip -cdivv data/gcc_full.tsv.lrz -O data/
-lrunzip -cdivv data/gcc_captions.txt.lrz -O data/
+lrunzip data/11-25-20_21-1500.tsv.lrz -o data/meme_data.tsv
+lrunzip data/gcc_full.tsv.lrz -O data/
+lrunzip data/gcc_captions.txt.lrz -O data/
