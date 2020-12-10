28790e9f6a34e3f48a944b52571e6542bf820b56
Samantha Zarate <slzarate@jhu.edu>
2020-12-10 13:07:37 -0500

    start autoscaling batch size to calculate num_training_steps

--- train.py
+++ train.py
@@ -9,18 +9,32 @@ def main(gpus: Optional[Union[int, str, List[int]]],
          accelerator: Optional[str],
          amp_backend: str,
          accumulate_grad_batches: Union[int, Dict[int, int], List[list]],
+         autoscale_batch_size: str,
          learning_rate: float,
          num_warmup_steps: int,
          num_epochs: int,
          weight_decay: float) -> None:
+
     img_flip = MemesDataModule()
+    
     model = GPT2(lr=learning_rate, num_warmup_steps=num_warmup_steps,
                  num_training_steps=num_training_steps, weight_decay=weight_decay)
     trainer = Trainer(gpus=gpus,
                       accelerator=accelerator,
                       amp_backend=amp_backend,
-                      accumulate_grad_batches=accumulate_grad_batches)
+                      accumulate_grad_batches=accumulate_grad_batches,
+                      auto_scale_batch_size = autoscale_batch_size)
+    tuner = Tuner(trainer)
+
+    if autoscale_batch_size is not None:
+        batch_size = tuner.scale_batch_size(model) # extra parameters here?
+    else:
+        batch_size = img_flip.batch_size()
+
+    model.hparams.num_training_steps = # todo
+
     trainer.fit(model, img_flip)
+
 
 
 if __name__ == "__main__":
@@ -34,6 +48,8 @@ if __name__ == "__main__":
                         default='native', help='which mixed precision backend to use ("native" or "apex")')
     parser.add_argument('-b', '--accumulate_grad_batches', type=Union[int, Dict[int, int], List[list]],
                         default=1, help="Accumulates gradients every k batches or as set up in the dict (in line with PyTorch Lightning's API")
+    parser.add_argument('-as', '--autoscale_batch_size', type=str,
+                        default=None, help='auto scale batch size? (None (no scaling), "power" scaling, or "binsearch" scaling)')
     parser.add_argument('-l', '--learning_rate', type=float,
                         default=5e-5, help='initial learning rate for AdamW optimizer')
     parser.add_argument('-w', '--num_warmup_steps', type=int,
