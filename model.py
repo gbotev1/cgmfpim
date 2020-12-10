126aabf159a842335e46b44cb489319d02a5947c
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-09 20:44:39 -0500

    Ready 4 take-off

--- model.py
+++ model.py
@@ -7,9 +7,11 @@ from typing import Dict, Union
 
 class GPT2(LightningModule):
 
-    def __init__(self, lr: float,
+    def __init__(self,
+                 lr: float,
                  num_warmup_steps: int,
                  num_training_steps: int,
+                 weight_decay: float,
                  gpt2_model_type: str = 'gpt2'):
         super(GPT2, self).__init__()
         self.tokenizer = GPT2TokenizerFast.from_pretrained(gpt2_model_type)
@@ -24,42 +26,37 @@ class GPT2(LightningModule):
         self.lr = lr
         self.num_warmup_steps = num_warmup_steps
         self.num_training_steps = num_training_steps
-        self.save_hyperparameters('lr')
+        self.weight_decay = weight_decay
+        self.save_hyperparameters('lr', 'weight_decay')
 
     def forward(self, inputs):
         return self.model(**inputs)
 
-    def train_step(self, batch: Dict[str, Union[torch.Tensor, int]], _) -> None:
-        # Try to predict input IDs by setting them as labels
+    def train_step(self, batch: Dict[str, Union[torch.Tensor, int]], batch_index: int) -> None:
+        # Try to predict input IDs by setting them as labels (verified approach in documentation)
         outputs = self(
             {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})
         loss = outputs[0]
-        self.log('train_loss', loss, on_step=True, on_epoch=True,
-                 prog_bar=True, logger=True, sync_dist=True)
+        self.log('train_loss', loss, prog_bar=True, sync_dist=True)
 
-    def val_step(self, batch: Dict[str, Union[torch.Tensor, int]], _) -> None:
-        # Try to predict input IDs by setting them as labels
+    def val_step(self, batch: Dict[str, Union[torch.Tensor, int]], batch_index: int) -> None:
+        # Try to predict input IDs by setting them as labels (verified approach in documentation)
         outputs = self(
             {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})
         loss = outputs[0]
-        self.log('train_loss', loss, on_step=True, on_epoch=True,
-                 prog_bar=True, logger=True, sync_dist=True)
+        self.log('val_loss', loss, prog_bar=True, sync_dist=True)
 
-    def test_step(self, batch: Dict[str, Union[torch.Tensor, int]], _) -> None:
-        # Try to predict input IDs by setting them as labels
+    def test_step(self, batch: Dict[str, Union[torch.Tensor, int]], batch_index: int) -> None:
+        # Try to predict input IDs by setting them as labels (verified approach in documentation)
         outputs = self(
             {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})
         loss = outputs[0]
+        self.log('test_loss', loss, prog_bar=True, sync_dist=True)
 
     def configure_optimizers(self):
         no_decay = ["bias", "LayerNorm.weight"]
-        if "weight_decay" in self.hparams:
-            weight_decay = self.hparams
-        else:
-            weight_decay = 0.0
-
         optimizer_grouped_parameters = [{"params": [p for n, p in self.model.named_parameters() if not any(nd in n for nd in no_decay)],
-                                         "weight_decay": weight_decay}]
+                                         "weight_decay": self.weight_decay}]
         optimizer = optim.AdamW(optimizer_grouped_parameters, lr=self.lr)
         scheduler = get_cosine_schedule_with_warmup(
             optimizer, self.num_warmup_steps, self.num_training_steps)
