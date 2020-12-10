bb8bc1e9494300087315d8030d32942641a70ada
Samantha Zarate <slzarate@jhu.edu>
2020-12-09 21:12:45 -0500

    Cleaning up and fleshing out training code slightly

--- train.py
+++ train.py
@@ -1,15 +1,39 @@
 import data
 from model import GPT2
 
-dataset = data.MemesDataModule() # data must be in data/meme_data.tsv (this is specified in data.py)
-dataset.prepare_data() # this generates a pickle
-dataset.setup() # generates dataloaders
+def prepare_data():
+    dataset = data.MemesDataModule() # data must be in data/meme_data.tsv (this is specified in data.py)
+    dataset.prepare_data() # this generates a pickle
+    dataset.setup() # generates dataloaders
 
-train_dl = dataset.train_dataloader()
-val_dl = dataset.val_dataloader()
-test_dl = dataset.test_dataloader()
+    train_dl = dataset.train_dataloader()
+    val_dl = dataset.val_dataloader()
+    test_dl = dataset.test_dataloader()
+    return train_dl, val_dl, test_dl
 
-# specify model
-GPT2_model = GPT2(lr=0.02, num_warmup_steps=1, num_training_steps=1, weight_decay=0.0)
+def define_model(learning_rate, num_warmup_steps, num_training_steps, weight_decay)
+    GPT2_model = GPT2(lr=learning_rate, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps, weight_decay=weight_decay)
+    return GPT2_model
 
-### TODO: specify loop over which to train/validate/test
+def train(train_dl, val_dl, test_dl, model):
+    ### TODO: fill in this code
+
+def main(learning_rate, num_warmup_steps, num_training_steps, weight_decay):
+    train_dl, val_dl, test_dl = prepare_data()
+    model = define_model(learning_rate, num_warmup_steps, num_training_steps, weight_decay)
+    train(train_dl, val_dl, test_dl, model)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Train GPT-2 model with pre-defined weights from HuggingFace on dataset as specified in data.py.",
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3, # TODO: tweak this default
+                        help='learning rate for model')
+    parser.add_argument('-w', '--num_warmup_steps', type=int, default=100, # TODO: tweak this default
+                        help='number of warmup steps') # TODO: make more descriptive
+    parser.add_argument('-t', '--num_training_steps', type=int, default=100, # TODO: tweak this default
+                        help='number of training steps') # TODO: make more descriptive
+    parser.add_argument('-wd', '--weight_decay', type=float, default=0.0, # TODO: tweak this default
+                        help="weight decay")
+    args = parser.parse_args()
+
+    main(learning_rate, num_warmup_steps, num_training_steps, weight_decay)
