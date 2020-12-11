d33661b8ec58907433c5d2b39914c28c5eca54f9
Samantha Zarate <slzarate@jhu.edu>
2020-12-11 01:11:45 -0500

    modifying gradient checkpointing

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
                         default=0, help='number of warmup steps')
     parser.add_argument('-d', '--weight_decay', type=float,
                         default=0.0, help="weight decay")
-    parser.add_argument('-c', '--gradient_checkpointing', type=bool, action="store_true",
-                        default=False, help="Whether or not to use gradient checkpointing to save memory at the expense of slower backward pass.")
+    parser.add_argument('-c', '--gradient_checkpointing', action="store_true",
+                        help="Whether or not to use gradient checkpointing to save memory at the expense of slower backward pass.")
     args = parser.parse_args()
     main(args)
