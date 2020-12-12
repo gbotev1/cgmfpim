d7d3eb5307bc36b07d753c17086ae6426a2388ec
Samantha Zarate <slzarate@jhu.edu>
2020-12-11 23:32:49 -0500

    modifications to generat

--- generate.py
+++ generate.py
@@ -4,17 +4,20 @@ import torch
 
 def main(args):
     # initialize tokenizer and model
-    tokenizer = GPT2TokenizerFast.from_pretrained('gpt2-medium')
-    model = GPT2DoubleHeadsModel.from_pretrained('gpt2-medium', pad_token_id=tokenizer.eos_token_id)
+    with open(args.checkpoint, 'r') as fn:
+        tokenizer = GPT2TokenizerFast.from_pretrained('gpt2-medium')
+        model = GPT2DoubleHeadsModel.from_pretrained('gpt2-medium', pad_token_id=tokenizer.eos_token_id)
 
-    # call this the way we called it recently
-    inputs = tokenizer.encode("One does not simply", return_tensors='pt')
+        # call this the way we called it recently
+        inputs = tokenizer.encode("One does not simply", return_tensors='pt')
 
-    # get model from checkpoint
-    output = model.generate(inputs, max_length=50, do_sample=True, top_p=0.95, top_k=50)
+        # get model from checkpoint
+        output = model.generate(inputs, max_length=50, do_sample=True, top_p=0.95, top_k=50)
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Generate meme captions',
                             formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--checkpoint', type=str,
+                        help='filename of checkpoint')
     args = parser.parse_args()
     main(args)
