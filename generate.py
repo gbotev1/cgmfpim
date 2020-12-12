b91aa24f728913573c65f64b3f83bda036c7a42c
Samantha Zarate <slzarate@jhu.edu>
2020-12-11 23:22:52 -0500

    adding main method to generate

--- generate.py
+++ generate.py
@@ -1,12 +1,20 @@
 from transformers import GPT2TokenizerFast, GPT2DoubleHeadsModel
 import torch
 
-# initialize tokenizer and model
-tokenizer = GPT2TokenizerFast.from_pretrained('gpt2-medium')
-model = GPT2DoubleHeadsModel.from_pretrained('gpt2-medium', pad_token_id=tokenizer.eos_token_id)
 
-# call this the way we called it recently
-inputs = tokenizer.encode("One does not simply", return_tensors='pt')
+def main(args):
+    # initialize tokenizer and model
+    tokenizer = GPT2TokenizerFast.from_pretrained('gpt2-medium')
+    model = GPT2DoubleHeadsModel.from_pretrained('gpt2-medium', pad_token_id=tokenizer.eos_token_id)
 
-# get model from checkpoint
-output = model.generate(inputs, max_length=50, do_sample=True, top_p=0.95, top_k=50)
+    # call this the way we called it recently
+    inputs = tokenizer.encode("One does not simply", return_tensors='pt')
+
+    # get model from checkpoint
+    output = model.generate(inputs, max_length=50, do_sample=True, top_p=0.95, top_k=50)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Generate meme captions',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    args = parser.parse_args()
+    main(args)
