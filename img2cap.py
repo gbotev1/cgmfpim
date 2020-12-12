0b4dbd3ea3cc89cdc0a862ed2f3cc6bb04c9afa4
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-11 19:20:18 -0500

    plz work

--- img2cap.py
+++ img2cap.py
@@ -26,7 +26,7 @@ class Wide_ResNet_101_2:
         with open(path.join(self.data_dir, captions)) as infile:
             self.captions = infile.readlines()
         self.embeddings = load(path.join(self.data_dir, embeddings))
-        self.index = faiss.IndexFlat(2048, faiss.METRIC_Canberra)
+        self.index = faiss.IndexFlat(2048, metric=faiss.METRIC_Canberra)
         self.index.add(self.embeddings)
 
         self.model = wide_resnet101_2(pretrained=True, progress=True)
