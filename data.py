347e90d557f42d61d7d19d2826cffe396dc72d2e
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-13 13:40:14 -0500

    improvements

--- data.py
+++ data.py
@@ -4,15 +4,16 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from os import path, cpu_count
 from csv import reader as csv_reader
 from typing import Optional, List
+from argparse import Namespace
+from pickle import load, dump, HIGHEST_PROTOCOL
 import torch
-import pickle
 
 
 class MemesDataset(Dataset):
 
     def __init__(self, path: str) -> None:
         with open(path, 'rb') as handle:
-            self.data = pickle.load(handle)
+            self.data = load(handle)
         self.num_memes = len(self.data)  # Save for efficiency
 
     def __len__(self) -> int:
@@ -24,11 +25,8 @@ class MemesDataset(Dataset):
 
 class MemesDataModule(LightningDataModule):
 
-    def __init__(self,
-                 args,
-                 split_ratios: List[float] = [0.8, 0.1, 0.1]) -> None:
+    def __init__(self, args: Namespace) -> None:
         super().__init__()
-        self.split_ratios = split_ratios
         self.gpu_boole = torch.cuda.is_available()
         self.num_cpus = cpu_count()
         # There should be no parallelism: stop warnings
@@ -46,6 +44,7 @@ class MemesDataModule(LightningDataModule):
 
     # prepare_data(): called first on MemesDataModule() object
     # produces pickle object at location data_dir/outfile
+    # do not assign to self in order to work for multi-GPU training!
     def prepare_data(self) -> None:
         captions = []
         with open(path.join(self.hparams.data_dir, self.hparams.infile), newline='') as tsvfile:
@@ -56,21 +55,21 @@ class MemesDataModule(LightningDataModule):
                 captions.append(
                     f'{meme[3]}{self.tokenizer.sep_token}{meme[2]}{self.tokenizer.eos_token}')
         with open(path.join(self.hparams.data_dir, self.hparams.outfile), 'wb') as handle:
-            pickle.dump(captions, handle, pickle.HIGHEST_PROTOCOL)
+            dump(captions, handle, HIGHEST_PROTOCOL)
 
     # get_splits(): called by setup() function
     # produces sizes of train/validation/test dataloaders for use later
     def get_splits(self, data_len: int) -> List[int]:
         # Error handling
-        if sum(self.split_ratios) != 1.0:
+        if sum(self.hparams.split_ratios) != 1.0:
             raise ValueError(
-                f'Split ratios "{self.split_ratios}" given has sum {sum(self.split_ratios)} instead of 1.0.')
+                f'Split ratios "{self.hparams.split_ratios}" given has sum {sum(self.hparams.split_ratios)} instead of 1.0.')
         if len(self.split_ratios) != 3:
             raise ValueError(
-                f'Split ratios "{self.split_ratios}" given has {len(self.split_ratios)} splits specified instead of 3 (corresponding to train-validation-test).')
+                f'Split ratios "{self.hparams.split_ratios}" given has {len(self.hparams.split_ratios)} splits specified instead of 3 (corresponding to train-validation-test).')
         # Make sure each split has an integral size
-        splits = [int(self.split_ratios[0] * data_len),
-                  int(self.split_ratios[1] * data_len)]
+        splits = [int(self.hparams.split_ratios[0] * data_len),
+                  int(self.hparams.split_ratios[1] * data_len)]
         splits.append(data_len - sum(splits))
         return splits
 
--- train.py
+++ train.py
@@ -6,10 +6,12 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, Namespace
 
 
 def main(args: Namespace) -> None:
-    seed_everything(0)  # For reproducability
+    if args.seed_everything:
+        seed_everything(0)  # For reproducability
     datamodule = MemesDataModule(args)
-    model = GPT2(args, datamodule.tokenizer)
+    model = GPT2(args=args, tokenizer=datamodule.tokenizer)
     trainer = Trainer.from_argparse_args(args, callbacks=[ProgressBar()])
+    trainer.tune(model, datamodule=datamodule)
     trainer.fit(model, datamodule)
 
 
@@ -21,10 +23,10 @@ if __name__ == '__main__':
                         default=5e-5, help='initial learning rate for AdamW optimizer')
     parser.add_argument('-b', '--batch_size', type=int,
                         default=1, help='initial batch size')
-    parser.add_argument('-w', '--weight_decay', type=float,
-                        default=0.0, help='weight decay')
     parser.add_argument('-c', '--gradient_checkpointing', action='store_true',
                         help='use gradient checkpointing to save memory at expense of slower backward pass')
+    parser.add_argument('-s', '--seed_everything', action='store_true',
+                        help="whether to call PyTorch Lightning's \"seed_everything\" method with argument 0 for reproducability")
     parser.add_argument('-g', '--gpt2_model_type', type=str, default='gpt2', choices=['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'],
                         help='pre-trained model ID string for GPT-2')
     parser.add_argument('-d', '--data_dir', type=str, default='data',
@@ -33,6 +35,6 @@ if __name__ == '__main__':
                         help='infile TSV name of meme data to initialize MemesDataModule')
     parser.add_argument('-o', '--outfile', type=str, default='data.pickle',
                         help='outfile TSV name of pickle file when MemesDataModule\'s "prepare_data" is called')
-    parser.add_argument('--num_training_steps', type=int, default=0,
-                        help='DO NOT CHANGE: will be automatically set but added here for code readability')
+    parser.add_argument('-r', '--split_ratios', nargs='+', type=float, default=[0.8, 0.1, 0.1],
+                        help='ratios of train, validation, and test dataloaders to use for training')
     main(parser.parse_args())
