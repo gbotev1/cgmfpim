4109b35fc8df18ee1ab51c8bb4f1b37964ddc6e2
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-12 21:30:55 -0500

    override LGTM non-callable error because we want our model to work for DataParallel

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class GPT2(LightningModule):
     def training_step(self, batch: Dict[str, Union[torch.Tensor, int]], batch_index: int) -> None:
         # Try to predict input IDs by setting them as labels (verified approach in documentation)
         outputs = self(
-            {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})
+            {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})  # lgtm [py/call-to-non-callable]
         loss = outputs[0]
         self.log('train_loss', loss, prog_bar=True, sync_dist=True)
         return outputs[0]
@@ -51,7 +51,7 @@ class GPT2(LightningModule):
     def validation_step(self, batch: Dict[str, Union[torch.Tensor, int]], batch_index: int) -> None:
         # Try to predict input IDs by setting them as labels (verified approach in documentation)
         outputs = self(
-            {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})
+            {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})  # lgtm [py/call-to-non-callable]
         loss = outputs[0]
         self.log('val_loss', loss, prog_bar=True, sync_dist=True)
         return loss
@@ -59,7 +59,7 @@ class GPT2(LightningModule):
     def test_step(self, batch: Dict[str, Union[torch.Tensor, int]], batch_index: int) -> None:
         # Try to predict input IDs by setting them as labels (verified approach in documentation)
         outputs = self(
-            {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})
+            {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'labels': batch['input_ids']})  # lgtm [py/call-to-non-callable]
         loss = outputs[0]
         self.log('test_loss', loss, prog_bar=True, sync_dist=True)
         return loss
