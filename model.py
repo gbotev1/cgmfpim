26fcde12105702ab45848f070c38632773bf3d06
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-13 22:31:29 -0500

    Removed unused import

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from transformers import PreTrainedTokenizerBase, GPT2LMHeadModel, get_cosine_schedule_with_warmup
+from transformers import PreTrainedTokenizerBase, GPT2LMHeadModel
 from pytorch_lightning import LightningModule
 import torch
 import torch.optim as optim
