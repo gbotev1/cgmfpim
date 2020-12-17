fc75ea6e6024fa2e572c77720e910bd5f0468edf
George Botev <12297345+gbotev1@users.noreply.github.com>
2020-12-16 23:41:02 -0500

    Bug champion

--- generate.py
+++ generate.py
@@ -19,7 +19,7 @@ def main(args: Namespace):
     model.eval()  # Don't forget to put model in evaluation mode!
     # Predict, switching based on generation type
     model = model if args.use_pretrained else model.model
-    outputs = model.generate(tokenizer.encode('Meme:\n\n'), eos_token_id=tokenizer.eos_token_id, do_sample=True,
+    outputs = model.generate(tokenizer.encode('Meme:\n\n', return_tensors='pt', padding=True, truncation=True), eos_token_id=tokenizer.eos_token_id, do_sample=True,
                              max_length=args.max_length, top_p=args.top_p, top_k=args.top_k, num_returned_sequences=args.num_returned_sequences)
     # Save and print results
     with open(args.outfile, 'w') as outfile:
